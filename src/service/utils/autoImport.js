1ebc9c7b975ff8ad18d3601cc6b994ea33572938
吳俊緯 <wcw850710@gmail.com>
2020-07-30 22:28:20 +0800

    自動導入 vue 格式修正

--- src/service/utils/autoImport.js
+++ src/service/utils/autoImport.js
@@ -2,16 +2,16 @@ const getToColorFilePath = require('./getToColorFilePath')
 const matchVueStartStyleTag = require('./getVueStyle').matchVueStartStyleTag
 
 module.exports = (extension, filePath, fileData, config) => {
-  const {isAutoImport, compileFileName} = config
+  const {isAutoImport, compileFileName, compileFileType} = config
   if(isAutoImport === true) {
     const isImportedColor = new RegExp(`@import\\s?['"]+.*${compileFileName}(\\.s[ac]ss)?['"]+[;\\s]*$`, 'gm').test(fileData)
     if (isImportedColor === false) {
-      const importSentence = `@import '${getToColorFilePath(filePath, config)}'${extension === 'scss' ? ';' : ''}\n`
+      const isVueFile = extension === 'vue'
+      const importSentence = `@import '${getToColorFilePath(filePath, config)}'${(extension === 'scss' || isVueFile && compileFileType === 'scss') ? ';' : ''}\n`
       if(extension === 'vue') {
         const matchStyleTag = fileData.match(matchVueStartStyleTag)
-        const { index, input } = matchStyleTag
-        const inputLen = input.length
-        fileData = input + '\n' + importSentence + + fileData.substring(inputLen)
+        const [styleTag] = matchStyleTag
+        fileData = fileData.replace(styleTag, styleTag + '\n' + importSentence)
       } else {
         fileData = importSentence + fileData
       }
