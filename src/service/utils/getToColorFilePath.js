8eb41e43c0d6f8a1d8cbf50351ef72f91afabd3c
吳俊緯 <wcw850710@gmail.com>
2020-08-01 00:17:30 +0800

    自動導路的路徑判斷錯誤修正

--- src/service/utils/getToColorFilePath.js
+++ src/service/utils/getToColorFilePath.js
@@ -8,55 +8,31 @@ module.exports = (sourcePath, {compileFilePath, compileFileName, compileFileType
   const filterLen = filterPath.length
   const compileLen = compilePathSp.length
   let resultPath = ``
-  // console.log(sourcePath, compileFilePath)
-  // console.log('sourcePathSp', sourcePathSp,
-  //   'compilePathSp', compilePathSp,
-  //   'filterPath', filterPath,)
-  if(filterLen < sourceLen && compileLen > filterLen) {
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\_color.sass
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\views\\a.sass
-    // ../assets/_color
-    // 路徑在 color 之後且不同資料結構
-    const minusLen = compileLen - filterLen
-    let path = ``
-    for (let i = 0; i < minusLen; i++) {
-      path += '../'
-    }
-    const minusLen2 = sourceLen - filterLen
-    const compSourceLen = sourceLen - minusLen2
-    let path2 = ``
-    for (let i = 0; i < minusLen2; i++) {
-      const _path = sourcePathSp[compSourceLen + i]
-      path += _path + '/'
-    }
-    resultPath = `${path}${path2}${compileFileName}.${compileFileType}`
-  } else if(filterLen + sourceLen + compileLen === compileLen * 3) {
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\_color.sass
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\a.sass
-    // ./_color
+  // console.log('sourcePathSp:', sourcePath, sourceLen, filterLen)
+  // console.log('compilePathSp:', compileFilePath)
+  if(sourceLen === compileLen && compileLen === filterLen) {
     // 同層的
     resultPath = `./${compileFileName}.${compileFileType}`
-  } else if(sourceLen > compileLen) {
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\_color.sass
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\haha\\jojo\\_color.sass
-    // ../../_color
-    // 超過 color 的資料夾
-    const minusLen = sourceLen - compileLen
+  } else if(filterLen < sourceLen) {
+    // 我們不一樣
     let path = ``
-    for (let i = 0; i < minusLen; i++) {
+    const minusSourceLen = sourceLen - filterLen
+    const minusCompileLen = compileLen - filterLen
+    for (let i = 0; i < minusSourceLen; i++) {
       path += '../'
     }
+    for (let i = 0; i < minusCompileLen; i++) {
+      const _path = compilePathSp[compileLen - minusCompileLen + i]
+      path += _path + '/'
+    }
     resultPath = `${path}${compileFileName}.${compileFileType}`
-  } else if(filterLen < sourceLen && compileLen === filterLen) {
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\src\\assets\\_color.sass
-    // C:\\Users\\wcw85\\desk\\code\\ws-projects\\sass-colors-compiler\\src\\gui\\c.sass
-    // ./src/assets/_color
+  } else if(filterLen === sourceLen && sourceLen < compileLen) {
     // 路徑在 color 之後且同資料結構
-    const minusLen = sourceLen - compileLen
-    const compSourceLen = sourceLen - minusLen
+    const minusLen = compileLen - filterLen
+    const compSourceLen = compileLen - minusLen
     let path = ``
     for (let i = 0; i < minusLen; i++) {
-      const _path = sourcePathSp[compSourceLen + i]
+      const _path = compilePathSp[compSourceLen + i]
       path += _path + '/'
     }
     resultPath = `./${path}${compileFileName}.${compileFileType}`
