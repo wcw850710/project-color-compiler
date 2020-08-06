0f5dedde8d7bc26fe08210fbd082bc74f8721927
吳俊緯 <wcw850710@gmail.com>
2020-08-06 22:18:50 +0800

    將 iColorList 定義拉出去

--- src/service/interfaces/color.ts
+++ src/service/interfaces/color.ts
@@ -1,1 +1,2 @@
 export type iGetColor = [string, boolean]
+export type iColorList = {color: string, variable: string, commit: string}[]
--- src/service/routes/getColors.ts
+++ src/service/routes/getColors.ts
@@ -3,12 +3,13 @@ import getConfig from '../utils/getConfig'
 import { iComputedConfig } from "../interfaces/config";
 import { iFileResult, iColorJSONContent } from "../utils/style/getFileColors";
 import {iExpressRoute} from "../interfaces/route";
+import {iColorList} from "../interfaces/color";
 
 const getColors: iExpressRoute = async (req, res) => {
   const { config } = req.body
   const { compileFilePath }: iComputedConfig = getConfig(config)
   const colorJson: iFileResult = await getFileColors(compileFilePath)
-  const colors: {color: string, variable: string, commit: string}[] = []
+  const colors: iColorList = []
   for (const color in colorJson) {
     const cJSON: {} | iColorJSONContent = colorJson[color]
     if(cJSON.hasOwnProperty('variable')) {
