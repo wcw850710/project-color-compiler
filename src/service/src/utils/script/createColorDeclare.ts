897a45ff5d98a6cebef19cf4377ef2280172e1a2
frank.wcw <frank.wcw@link8tech.tw>
2020-08-08 13:07:02 +0800

    新增 suffix 可選參數

--- src/service/src/utils/script/createColorDeclare.ts
+++ src/service/src/utils/script/createColorDeclare.ts
@@ -4,20 +4,20 @@ type iColors = {
   [color: string]: string
 }
 
-export const createColorDeclare = (config: iComputedConfig, colors: iColors) => {
+export const createColorDeclare = (config: iComputedConfig, colors: iColors, suffix: string = "',") => {
   const {compileFileType, compileFileName}: iComputedConfig = config
   const fileName: string = compileFileName
   const colorsKeys: string[] = Object.keys(colors)
   let result = `export const ${fileName}`
-  if(compileFileType === 'ts') {
+  if (compileFileType === 'ts') {
     result = `interface iColor { [variable: string]: string }\n\n${result}: iColor`
   }
   result += ` = {`
-  if(colorsKeys.length > 0) {
+  if (colorsKeys.length > 0) {
     result += '\n'
     colorsKeys.forEach(color => {
       const variable = colors[color]
-      result += `  ${variable}: '${color}',\n`
+      result += `  ${variable}: '${color}${suffix}\n`
     })
   }
   result += '}'
