f9c1245da6cd06688aba4c2b99cc490f8eb54f37
frank.wcw <frank.wcw@link8tech.tw>
2020-08-08 13:07:23 +0800

    新增 js 顏色變量轉換功能

--- src/service/src/routes/translateColorsAndVariables/script/index.ts
+++ src/service/src/routes/translateColorsAndVariables/script/index.ts
@@ -1,24 +1,29 @@
 import * as fs from 'fs'
 import recursiveDir from '../../../utils/recursiveDir'
-import createHash from '../../../utils/createHash'
 import {iComputedConfig} from "../../../interfaces/config";
 import {iTranslateColorList} from "../../../interfaces/color";
 import {iReturnResponse} from "../../../interfaces/resolve";
+import {createColorDeclare} from "../../../utils/script/createColorDeclare";
 
 interface iConfigAndColor {
   config: iComputedConfig
   colors: iTranslateColorList
 }
 
+type iColors = {
+  [color: string]: string
+}
+
 export const translateScript = ({config, colors}: iConfigAndColor): Promise<iReturnResponse> => new Promise((resolve) => {
-  const { compileFilePath, compileFileType }: iComputedConfig = config
+  const { compileFilePath, compileFileName }: iComputedConfig = config
 
   // 覆蓋顏色
-  const colorsString: string = colors.reduce((prev, {color, newVariable, commit}, index) => {
-    const data: string = `$${newVariable.trim() !== '' ? newVariable.trim() : createHash()}: ${color.trim() === '' ? '#000' : color}${compileFileType === 'scss' ? ';' : ''}${commit !== '' ? `// ${commit}`: ''}`
-    return index === 0 ? data : prev + `\n${data}`
-  }, '')
-  fs.writeFile(compileFilePath, colorsString, _ => {})
+  const cacheColors = colors.reduce<iColors>((prev: iColors, {newVariable, color, commit}) => ({
+    ...prev,
+    [`${color}', ${commit !== '' ? `// ${commit}`: ''}`]: newVariable,
+  }), {})
+  const declaredFileData = createColorDeclare(config, cacheColors, '')
+  fs.writeFile(compileFilePath, declaredFileData, _ => {})
 
   // 遍歷調色變量
   let cacheFileLength = 0
@@ -26,8 +31,8 @@ export const translateScript = ({config, colors}: iConfigAndColor): Promise<iRet
   const replaceVariables = (data: string, path: string) => {
     colors.forEach(({ newVariable, oldVariable }) => {
       if(oldVariable !== '' && oldVariable !== newVariable) {
-        while(data.search(`\\$${oldVariable}`) !== -1) {
-          data = data.replace(new RegExp(`\\$${oldVariable}`, 'gm'), `$${newVariable}`)
+        while(data.search(`${compileFileName}.${oldVariable}`) !== -1) {
+          data = data.replace(new RegExp(`${compileFileName}.${oldVariable}`, 'gm'), `${compileFileName}.${newVariable}`)
         }
       }
     })
